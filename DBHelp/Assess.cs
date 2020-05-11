56b28fc3e126d1d00128846bf0d3bca242584ab9
Lhb <17673856039@qq.com>
2020-05-11 15:47:15 +0800

    push

---
+++ DBHelp/Assess.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace DBHelp
+{
+    /// <summary>
+    /// 连接Sqlserver工具类
+    /// </summary>
+    public class Assess
+    {
+        /// <summary>
+        /// 连接数据库字符串
+        /// </summary>
+        private static string conString =
+           System.Configuration.ConfigurationManager.ConnectionStrings["conString"].ConnectionString;
+
+        /// <summary>
+        /// 执行增删改的方法
+        /// </summary>
+        /// <param name="sql">sql语句</param>
+        /// <param name="pms">传入的参数</param>
+        /// <returns>返回受影响的行数</returns>
+        public static int ExecuteNonQuery(string sql,params SqlParameter[] parameters)
+        {
+            using (SqlConnection con = new SqlConnection(conString))
+            {
+                using (SqlCommand com = con.CreateCommand())
+                {
+                    con.Open();
+                    com.CommandText = sql;
+                    com.Parameters.AddRange(parameters);
+                    return com.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public static int ExecuteNonQuery(string sql)
+        {
+            using (SqlConnection con = new SqlConnection(conString))
+            {
+                using (SqlCommand com = con.CreateCommand())
+                {
+                    con.Open();
+                    com.CommandText = sql;
+                    return com.ExecuteNonQuery();
+                }
+            }
+        }
+
+        /// <summary>
+        /// 执行查询，返回单个值
+        /// </summary>
+        /// <param name="sql"> 传入的sql语句字符串</param>
+        /// <param name="pms">sql语句中的参数</param>
+        /// <returns>返回一个Object</returns>
+        public static object ExecuteScalar(string sql,CommandType type, params SqlParameter[] parameters)
+        {
+            using (SqlConnection conn = new SqlConnection(conString))
+            {
+                using (SqlCommand comm = conn.CreateCommand())
+                {
+                    conn.Open();
+                    comm.CommandType = type;
+                    comm.CommandText = sql;
+                    comm.Parameters.AddRange(parameters);
+                    return comm.ExecuteScalar();
+                }
+            }
+        }
+
+        /// <summary>
+        /// 无存储过程的ExecuteScalar()方法
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <returns></returns>
+        public static object ExecuteScalar(string sql)
+        {
+            using (SqlConnection conn = new SqlConnection(conString))
+            {
+                using (SqlCommand com = conn.CreateCommand())
+                {
+                    conn.Open();
+                    com.CommandText = sql;
+                    return com.ExecuteScalar();
+                }
+            }
+        }
+
+        /// <summary>
+        /// 执行查询，返回多个值
+        /// </summary>
+        /// <param name="sql">sql语句</param>
+        /// <returns>返回一个Reader,用于获取返回的每个结果</returns>
+        public static SqlDataReader ExecuteReader(string sql, params SqlParameter[] parameters)
+        {
+            //SqlDataReader要求，它读取数据的时候有，它独占它的SqlConnection对象，而且SqlConnection必须是Open状态
+            SqlConnection conn = new SqlConnection(conString);//不要释放连接，因为后面还需要连接打开状态
+            SqlCommand cmd = conn.CreateCommand();
+            conn.Open();
+            cmd.CommandText = sql;
+            cmd.Parameters.AddRange(parameters);
+            //CommandBehavior.CloseConnection当SqlDataReader释放的时候，顺便把SqlConnection对象也释放掉
+            return cmd.ExecuteReader(CommandBehavior.CloseConnection);
+        }
+
+        /// <summary>
+        /// 查询操作
+        /// </summary>
+        /// <param name="sql">sql语句</param>
+        /// <returns>返回DataTable</returns>
+        public static DataTable ExcuteDatable(string sql, CommandType type,params SqlParameter[] parameters)
+        {
+        
+            using (SqlConnection con = new SqlConnection(conString))
+            {
+                SqlCommand com = con.CreateCommand();
+                con.Open();
+                com.CommandType = type;
+                com.CommandText = sql;
+                com.Parameters.AddRange(parameters);
+                SqlDataAdapter da = new SqlDataAdapter(com);
+                DataTable db = new DataTable();
+                da.Fill(db);
+                return db;
+            }
+        }
+
+        /// <summary>
+        /// 无存储过程返回受影响行数的方法
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <returns></returns>
+        public static DataTable ExecuteQuery(string sql)
+        {
+            using (SqlConnection conn = new SqlConnection(conString))
+            {
+                conn.Open();
+                SqlCommand comm = new SqlCommand(sql, conn);
+                DataTable dt = new DataTable();
+                SqlDataAdapter ada = new SqlDataAdapter(comm);
+                ada.Fill(dt);
+                return dt;
+            }
+        }
+
+
+
+
+        /// <summary>
+        /// 执行存储过程
+        /// </summary>
+        /// <param name="procName"></param>
+        /// <param name="outPara"></param>
+        /// <param name="p"></param>
+        public static void RunProc(string procName,SqlParameter outPara,params SqlParameter[] p)
+        {
+            using (SqlConnection con = new SqlConnection(conString))
+            {
+                con.Open();
+                SqlCommand com = new SqlCommand(procName,con);
+                //存储过程
+                com.CommandType = CommandType.StoredProcedure;
+                //指定参数的方向
+                outPara.Direction = ParameterDirection.Output;
+                com.Parameters.Add(outPara);  //添加一个输出参数
+               
+                com.Parameters.AddRange(p);
+                com.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ TableConvert/TableConvert/DataTable2Objects.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TableConvert
+{
+    /// <summary>
+    /// 泛型类
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class DataTable2Objects<T> where T : new()
+    {
+        private static List<T> ConvertToEntity(DataTable dt, IEnumerable<PropertyInfo> propertys)
+        {
+            var list = new List<T>();
+            //遍历DataTable中所有的数据行  
+            foreach (DataRow dr in dt.Rows)
+            {
+                var entity = new T();
+
+                //遍历该对象的所有属性  
+                foreach (PropertyInfo p in propertys)
+                {
+                    //将属性名称赋值给临时变量
+                    string tmpName = p.Name;
+
+                    //检查DataTable是否包含此列（列名==对象的属性名）    
+                    if (!dt.Columns.Contains(tmpName)) continue;
+                    //取值  
+                    object value = dr[tmpName];
+                    //如果非空，则赋给对象的属性  
+                    if (value != DBNull.Value)
+                    {
+                        p.SetValue(entity, value, null);
+                    }
+                }
+                //对象添加到泛型集合中  
+                list.Add(entity);
+            }
+            return list;
+        }
+   
+        public  static T ConvertToEntity(DataTable dt)
+        {
+            DataTable dtTable = dt.Clone();
+            dtTable.Rows.Add(dt.Rows[0].ItemArray);
+            return ConvertToList(dtTable)[0];
+        }
+
+        public static  List<T> ConvertToList(DataTable dt)
+        {
+            // 定义集合  
+            var list = new List<T>();
+
+            if (0 == dt.Rows.Count)
+            {
+                return list;
+            }
+
+            // 获得此模型的可写公共属性  
+            IEnumerable<PropertyInfo> propertys = new T().GetType().GetProperties().Where(u => u.CanWrite);
+            list = ConvertToEntity(dt, propertys);
+
+
+            return list;
+        }
+
+    }
+
+}
+
+
+
+ 
